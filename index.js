e43928573071faccbd4985c6cd81da7ddf465ea8
Fiol Thiago <fiolthiago9@gmail.com>
2024-05-26 19:39:58 -0300

    añadimos los archivos principales y algunas librerias, framework que nos va a servir para mas adelante

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import 'dotenv/config';
+import {fileURLToPath} from 'url'
+import path from 'path';
+import productosRouter from "./src/routes/productos.routes.js";
+import pedidosRouter from './src/routes/pedidos.routes.js';
+import usuariosRouter from './src/routes/usuarios.routes.js';
+import './src/database/database.js'
+
+const app = express()
+app.set('port',process.env.PORT ||4000);
+app.listen(app.get('port'),()=>{
+    console.log('Estoy en el puerto '+app.get('port'))
+})
+
+app.use(cors()); 
+app.use(morgan('dev')) 
+app.use(express.json()) 
+app.use(express.urlencoded({extended:true}))
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static((path.join(__dirname,'/public'))))
