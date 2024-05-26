7911d01d3d98bacfe338e35bea53e14869c67ade
Fiol Thiago <fiolthiago9@gmail.com>
2024-05-26 19:57:56 -0300

    añadimos las ultimas librerias que vamos a utilizar y tambien añadimos unos retoques en los archivos

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import cors from 'cors';
 import 'dotenv/config';
 import {fileURLToPath} from 'url'
 import path from 'path';
-import productosRouter from "./src/routes/productos.routes.js";
-import pedidosRouter from './src/routes/pedidos.routes.js';
-import usuariosRouter from './src/routes/usuarios.routes.js';
-import './src/database/database.js'
+// import productosRouter from "./src/routes/productos.routes.js";
+// import pedidosRouter from './src/routes/pedidos.routes.js';
+// import usuariosRouter from './src/routes/usuarios.routes.js';
+// import './src/database/database.js'
 
 const app = express()
 app.set('port',process.env.PORT ||4000);
